4a3bd70c49308c32130cd476625aac9feefeb679
Victor Cazanave <victor.cazanave@gmail.com>
2019-04-06 19:45:02 +0800

    [Maps] Generate JS files and create index file

---
+++ index.js
@@ -0,0 +1,13 @@
+import Australia from './maps/australia/autralia'
+import FranceRegions from './maps/france/france.regions'
+import Taiwan from './maps/taiwan/taiwan'
+import TaiwanMain from './maps/taiwan/taiwan.main'
+import USA from './maps/usa/usa'
+
+export default {
+  Australia,
+  FranceRegions,
+  Taiwan,
+  TaiwanMain,
+  USA,
+}
